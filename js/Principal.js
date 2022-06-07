3c964d4b9e61fd0103e6b5a7cc9ed87ef955f95d
John Soares <johnsued25@gmail.com>
2022-06-07 10:19:42 -0300

    first commit

---
+++ js/Principal.js
@@ -0,0 +1,32 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-pasciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválido");
+    alturaEhValida = false;
+    tdImc.textContent = "altura inválida!";
+}
+
+if(alturaEhValida && pesoEhValido){
+    var imc = peso /(altura * altura);
+tdImc.textContent = imc;
+}
